d20dbf2a1396465a9ea6da9298534fa4d07bc80c
XiaofanDelta <xiaofanmcplayer@outlook.com>
2026-09-06 11:39:28 +0800

    Initial Commit

---
+++ src/main/java/nothing/xiaofan_delta/cmc/Cmc.java
@@ -0,0 +1,18 @@
+package nothing.xiaofan_delta.cmc;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Cmc.MODID)
+public class Cmc {
+	public static final String MODID = "cmc";
+	public static final String NAME = "Create More Chunks";
+	public static final Logger LOGGER = LogUtils.getLogger();
+
+	// 工具方法：获取模组命名空间下的资源路径
+	public static ResourceLocation loadResource(String path) {
+		return ResourceLocation.fromNamespaceAndPath(MODID, path);
+	}
+
+}
